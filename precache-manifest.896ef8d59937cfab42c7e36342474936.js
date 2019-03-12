5b6b40ae57028db4d5d8b6415e6fcadb6c2e70c8
Pavel Karpenko <ospkiev@gmail.com>
2019-03-12 16:07:04 +0200

    Updates

--- precache-manifest.896ef8d59937cfab42c7e36342474936.js
+++ precache-manifest.896ef8d59937cfab42c7e36342474936.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/test_text_editor/static/js/runtime~main.84436264.js"
   },
   {
-    "revision": "edd27d2ffb19cd4b89a6",
-    "url": "/test_text_editor/static/js/main.edd27d2f.chunk.js"
+    "revision": "e4b689254fbb435d27e2",
+    "url": "/test_text_editor/static/js/main.e4b68925.chunk.js"
   },
   {
     "revision": "1cf634af0a754b5e78be",
     "url": "/test_text_editor/static/js/2.1cf634af.chunk.js"
   },
   {
-    "revision": "edd27d2ffb19cd4b89a6",
+    "revision": "e4b689254fbb435d27e2",
     "url": "/test_text_editor/static/css/main.87112c23.chunk.css"
   },
   {
-    "revision": "c7111018e87403c2e69a149e28aae06e",
+    "revision": "511254883d4277dedfc4cbc604632066",
     "url": "/test_text_editor/index.html"
   }
 ];

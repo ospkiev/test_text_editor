ef7e5234b266ef3e8b719c7504553f2eab9868e4
Pavel Karpenko <ospkiev@gmail.com>
2019-03-12 13:42:14 +0200

    2 commit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,98 @@
 import React, { Component } from 'react';
+// import { Editor } from 'react-draft-wysiwyg';
+import { EditorState } from 'draft-js';
+import axios from 'axios';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import './App.css';
+
 
 
 class App extends Component {
 
   state = {
-    inputValue: '',
-    toArr: [],
-    selectWord: []
-  }
+    editorState: EditorState.createEmpty(),
+    selectWord: '',
+    arraySyn: [],
+    word: '',
+    replaceWord: '',
+  };
 
-  inputFunc = (e) => {
-    let value = e.target.value;
-    // let arr = value.split(' ');
-    this.setState({
-      inputValue: value,
-    })
-  }
 
-  getSelectionText = () => {
+  getSyn = () => {
+    let synWord = this.state.selectWord;
+    axios.get(`https://api.datamuse.com//words?rel_syn=${synWord}`)
+      .then(res => res.data)
+      .then(res => {
+        this.setState({
+          arraySyn: [...res],
+        })
+      })
+      .catch(error => console.log(error))
+  };
+
+
+  // onEditorStateChange = async (editorState) => {
+  //   console.log(editorState);
+  //   this.setState({
+  //     editorState,
+  //   });
+  //   await this.getSelectionText();
+  //   this.getSyn();
+  // };
+
+
+  getSelectText = async (e) => {
     let txt = window.getSelection().toString();
-    console.log(txt);
-    this.setState({
+    await this.setState({
       selectWord: txt,
+    });
+    this.getSyn();
+  };
+
+
+  targetWord = (e) => {
+    let syncWord = e.target.dataset.name;
+    this.setState({
+      word: syncWord,
     })
-  }
+  };
+
+
+  keyHandler = (e) => {
+    let j = e.key;
+    console.log(j);
+    this.setState(prev => ({
+      word: prev.word + j,
+    }))
+  };
+
+
+
+  // getCoord = (e) => {
+  //   const coords = e.target.getBoundingClientRect();
+  //   console.log(coords);
+  //   console.log('width :' + coords.width);
+  // }
+
 
   render() {
 
-    const { inputValue, toArr, selectWord } = this.state;
+    const { editorState, arraySyn, word } = this.state;
     return (
       <div className="App">
-        <div className='title'>Text editor</div>
-        <form action="submit" >
-          {/* <textarea rows="10" cols="45" name="text" onChange={this.inputInfo} value={this.state.inputValue} onDoubleClick={this.getSelectionText}>{selectWord}</textarea> */}
-          <input type='text' value={this.state.inputValue} onChange={this.inputFunc} onDoubleClick={this.getSelectionText} id='input'></input>
-          <p className='targetword'>{selectWord}</p>
-          <div className='modal_window'>Text editor</div>
-        </form>
+        {arraySyn.length !== 0 ? <div className='modal_of_syn' >{arraySyn.slice(0, 6).map(el => <p className='word_syn' data-name={el.word} onClick={this.targetWord}>{el.word}</p>)}</div> : null}
+        {/* <Editor editorState={editorState} toolbarClassName="toolbarClassName" wrapperClassName="wrapperClassName" editorClassName="editorClassName"
+          onEditorStateChange={this.onEditorStateChange} className='editor' /> */}
+        <div className='label_wrapper'>
+          <label for='check_bold' className='bold'>BOLD</label>
+          <label for='check_underline' className='underline'>UNDERLINE</label>
+          <label for='check_italic' className='italic'>ITALIC</label>
+        </div>
+        <input type='checkbox' name='input' id='check_bold' />
+        <input type='checkbox' name='input' id='check_underline' />
+        <input type='checkbox' name='input' id='check_italic' />
+
+        <div onKeyPress={this.keyHandler} tabIndex={-1} onClick={this.getSelectText} className='bold italic underline textfield'>{word}</div>
       </div >
     )
   }

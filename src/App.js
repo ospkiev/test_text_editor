6eba6a0d3326cd3e85a39f98930c48691c30c870
Pavel Karpenko <ospkiev@gmail.com>
2019-03-12 15:26:03 +0200

    predeploy commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-// import { Editor } from 'react-draft-wysiwyg';
-import { EditorState } from 'draft-js';
 import axios from 'axios';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import './App.css';
 
 
@@ -10,7 +7,6 @@ import './App.css';
 class App extends Component {
 
   state = {
-    editorState: EditorState.createEmpty(),
     selectWord: '',
     arraySyn: [],
     word: '',
@@ -31,16 +27,6 @@ class App extends Component {
   };
 
 
-  // onEditorStateChange = async (editorState) => {
-  //   console.log(editorState);
-  //   this.setState({
-  //     editorState,
-  //   });
-  //   await this.getSelectionText();
-  //   this.getSyn();
-  // };
-
-
   getSelectText = async (e) => {
     let txt = window.getSelection().toString();
     await this.setState({
@@ -59,40 +45,43 @@ class App extends Component {
 
 
   keyHandler = (e) => {
-    let j = e.key;
-    console.log(j);
-    this.setState(prev => ({
-      word: prev.word + j,
-    }))
+    let character = e.key;
+    if (character === 'Backspace') {
+      let arrOfWord = this.state.word.split('');
+      arrOfWord.pop();
+      arrOfWord = arrOfWord.join('');
+      this.setState({
+        word: arrOfWord,
+      })
+    } else {
+      this.setState(prev => ({
+        word: prev.word + character,
+      }))
+    }
   };
-
-
-
-  // getCoord = (e) => {
-  //   const coords = e.target.getBoundingClientRect();
-  //   console.log(coords);
-  //   console.log('width :' + coords.width);
-  // }
-
 
   render() {
 
-    const { editorState, arraySyn, word } = this.state;
+    const { arraySyn, word } = this.state;
+
     return (
+
       <div className="App">
-        {arraySyn.length !== 0 ? <div className='modal_of_syn' >{arraySyn.slice(0, 6).map(el => <p className='word_syn' data-name={el.word} onClick={this.targetWord}>{el.word}</p>)}</div> : null}
-        {/* <Editor editorState={editorState} toolbarClassName="toolbarClassName" wrapperClassName="wrapperClassName" editorClassName="editorClassName"
-          onEditorStateChange={this.onEditorStateChange} className='editor' /> */}
+
         <div className='label_wrapper'>
           <label for='check_bold' className='bold'>BOLD</label>
           <label for='check_underline' className='underline'>UNDERLINE</label>
           <label for='check_italic' className='italic'>ITALIC</label>
         </div>
+
         <input type='checkbox' name='input' id='check_bold' />
         <input type='checkbox' name='input' id='check_underline' />
         <input type='checkbox' name='input' id='check_italic' />
 
-        <div onKeyPress={this.keyHandler} tabIndex={-1} onClick={this.getSelectText} className='bold italic underline textfield'>{word}</div>
+        <div onKeyDown={this.keyHandler} tabIndex={-1} onClick={this.getSelectText} className='bold italic underline textfield'>{word}</div>
+
+        {arraySyn.length !== 0 ? <div className='modal_of_syn' >{arraySyn.slice(0, 6).map(el => <p className='word_syn' data-name={el.word} onClick={this.targetWord}>{el.word}</p>)}</div> : null}
+
       </div >
     )
   }
